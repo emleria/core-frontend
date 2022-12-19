73b7b9b8d6fd48395171167853a65371807b28f1
Michal Jeziorkowski <michaljeziorkowski@gmail.com>
2022-12-19 01:08:14 +0100

    Add tailwind and sass

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
-    <h1>Elmeria</h1>
+    <h1 class="text-3xl font-bold underline">Hello Elmeria</h1>
   </div>
 </template>
+
+<styles lang="scss" scoped>
+h1 {
+  color: red;
+}
+</styles>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,11 @@ export default defineNuxtConfig({
   devServer: {
     port: parseInt(process.env.PORT) || 3000,
   },
+  css: ["~/assets/css/main.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 });
